8d031ead68ba119f8ecdb1450132bcea748b8f4e
Firmino Andre <andrezfirmino@gmail.com>
2021-08-20 17:20:09 -0300

    corrections

--- js/main.js
+++ js/main.js
@@ -2,75 +2,80 @@
 var minutes = 25;
 var seconds = "00";
 var jaFoiClicado = false; // começa com false pra poder ser clicado pela primeira vez
+console.log(jaFoiClicado);
 
 // run sounds in every action
 var play = new Audio("assets/songs/run.mp3");
 var replay = new Audio("assets/songs/replay.mp3");
 var finish = new Audio("assets/songs/finish.mp3");
 
+
 // insert visualy variable minutes and seconds in html
 function template() {
     document.getElementById("minutes").innerHTML = "00";
     optionSelect = document.getElementById("select").value;
-    if(optionSelect == 4){
+    if (optionSelect == 4) {
         optionSelect = 5
         document.getElementById("minutes").innerHTML = optionSelect;
         return;
     }
-    if(optionSelect == 9){
+    if (optionSelect == 9) {
         optionSelect = 10
         document.getElementById("minutes").innerHTML = optionSelect;
         return;
     }
-    if(optionSelect == 14){
+    if (optionSelect == 14) {
         optionSelect = 15
         document.getElementById("minutes").innerHTML = optionSelect;
         return;
     }
-    if(optionSelect == 19){
+    if (optionSelect == 19) {
         optionSelect = 20
         document.getElementById("minutes").innerHTML = optionSelect;
         return;
     }
-    if(optionSelect == 24){
+    if (optionSelect == 24) {
         optionSelect = 25
         document.getElementById("minutes").innerHTML = optionSelect;
         return;
     }
-    
+
     document.getElementById("seconds").innerHTML = seconds;
 
 }
+
 
 function mostrarValor() {
     document.getElementById("secao").value;
 }
 
 
-//
-
-function clickButton(){
+function clickButton() {
     var div = document.getElementById("btnPause");
     div.style.display = "none";
-    if(!jaFoiClicado){
+    if (!jaFoiClicado) { // clicou no botao primeira vez vai verificar se o input ta vazio
         verifyInput();
-        jaFoiClicado =  true;
+        jaFoiClicado = true;
+        console.log(jaFoiClicado)
         return;
     }
-    alert("Espere o tempo acabar");
+    $('#alertModal').modal('show')
+     
 }
 
 
-function verifyInput(){
+function verifyInput() {
     var input = document.getElementById("secao").value;
-    if (input.length === 0 || !input.trim() ){
-        alert("Preencha o campo de seção");
+    if (input.length === 0 || !input.trim()) {
+        $('#alertModal2').modal('show')
+        
         return;
     }
 
     start();
+    
+    
 }
-
 
 
 function start() {
@@ -101,23 +106,26 @@ function start() {
                 document.getElementById("done").innerHTML = "Pomodoro completo com sucesso! Dê um intervalo de 5min";
 
                 document.getElementById("done").classList.add("showMsg");
+                
+                var tabela = document.getElementById("tabelinha");
+                tabela.style.display = "block";
 
 
                 var table = document.getElementById("table");
                 var numberLines = table.rows.length;
-                var line =  table.insertRow(numberLines); 
+                var line = table.insertRow(numberLines);
                 var section = document.getElementById("secao").value;
                 var local = localStorage.getItem('user');
 
-               
-                
+
+
 
                 var user = line.insertCell(0);
                 var pomodoroSection = line.insertCell(1);
 
                 //user.innerHTML = "User 1";
                 //nome = pomodoroSection.innerHTML = section;
-                
+
                 localStorage.setItem('user', user.innerHTML = "User 1");
                 localStorage.setItem('name', pomodoroSection.innerHTML = section);
 
